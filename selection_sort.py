564618cb5c6eef272d4e36510a4f50fb75c4dde3
choeyunseo <choeyunseo4060@naver.com>
2023-08-01 20:25:34 +0900

    Add selection sort

---
+++ selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(arr):
+    for i in range(len(arr) - 1):
+        min_idx = i
+        for j in range(i, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[min_idx], arr[i] = arr[i], arr[min_idx]
+    return arr
+
+arr = [5, 4, 3, 2, 1]
+print(selection_sort(arr))
