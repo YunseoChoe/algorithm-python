93e62a5b8bd605ae443b87eeb8958dbbed7a07f6
choeyunseo <choeyunseo4060@naver.com>
2023-08-01 20:35:41 +0900

    Add bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+arr = [5, 4, 3, 2, 1]
+print(bubble_sort(arr))
