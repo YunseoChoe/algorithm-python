f9fe5afe208deca356f5ed3ebabf5c819d6ca913
choeyunseo <choeyunseo4060@naver.com>
2023-08-03 14:31:05 +0900

    Add insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,14 +1,13 @@
 def insertion_sort(arr):
     for i in range(1, len(arr)):
-        for j in range(i):
-            if arr[j] > arr[i]:
-                arr[j], arr[i] = arr[i], arr[j]
+        for j in range(i, 0, -1):
+            if arr[j - 1] > arr[j]:
+                arr[j - 1], arr[j] = arr[j], arr[j - 1]
 
     return arr
 
-arr = [6, 4, 5, 2, 3, 1]
+arr = [6, 4, 3, 2, 5, 1]
 print(insertion_sort(arr))
-
 
 # arr = [5, 4, 3, 2, 1]
 
@@ -18,3 +17,4 @@ print(insertion_sort(arr))
 #             arr[j - 1], arr[j] = arr[j], arr[j - 1]
 
 # print(arr)
+
