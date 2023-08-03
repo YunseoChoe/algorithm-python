3575e1f3bc0d9f30e371933357ecee4b0e9679f3
choeyoonseo <128898996+choeyoonseo@users.noreply.github.com>
2023-08-03 14:31:53 +0900

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -9,12 +9,3 @@ def insertion_sort(arr):
 arr = [6, 4, 3, 2, 5, 1]
 print(insertion_sort(arr))
 
-# arr = [5, 4, 3, 2, 1]
-
-# for i in range(1, len(arr)):
-#     for j in range(i, 0, -1):
-#         if arr[j - 1] > arr[j]:
-#             arr[j - 1], arr[j] = arr[j], arr[j - 1]
-
-# print(arr)
-
